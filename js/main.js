bddc85e2dc8e7cb1e5b67fbc72cb0228307f858f
Andrej Maier <88maier@mail.ru>
2021-06-01 00:00:48 +0200

    Подключил js файл и сделал первые функции

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function getRandomNumber (min, max) {
+  if (min < 0 || max < 0) {
+    throw 'Видены не правильные значения. Число не может быть отрицательным';
+  } else if (min > max) {
+    throw 'Значение "от" не может быть больше значения "до". Пожалуйста исправте.';
+  } else if (min === max) {
+    throw 'Мне здесь нечего высчислять!';
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+getRandomNumber(0, 60);
+
+function stringValidation (text, maxTextLenght) {
+  if (text.length > maxTextLenght) {
+    return false;
+  }
+  return true;
+}
+
+stringValidation('Привет', 3);
